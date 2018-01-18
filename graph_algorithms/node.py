f40b36386058a55276ed6517c6c4fd92b9db5a38
Niels-Christian F. Bagger <nbag@dtu.dk>
2018-01-18 14:28:58 +0100

    Added functions to report statistics about a graph

--- graph_algorithms/node.py
+++ graph_algorithms/node.py
@@ -21,6 +21,7 @@
 # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+import math
 
 class Node:
     def __init__(self, name: str = ""):
@@ -34,3 +35,52 @@ class Node:
     
     def __repr__(self):
         return self.name
+
+
+def print_statistics(nodes: [Node]):
+    print("Graph statistics")
+    edges = math.floor(sum(len(n.adjacent) for n in nodes) / 2)
+    density = edges / len(nodes) / (len(nodes) + 1)
+    print("  Nodes      : {:8}\n  Edges      : {:8}\n  Density    : {:8.2%}".format(len(nodes),edges,density))
+    print("  Expected maximal cliques in random graph = {}".format(
+        expected_maximal_cliques_in_random_graph(len(nodes),density)))
+    queue = []
+    visited = set()
+    components = []
+    for node in nodes:
+        if node in visited:
+            continue
+        queue.append(node)
+        visited.add(node)
+        new_component = []
+        while len(queue) > 0:
+            next_node = queue.pop()
+            new_component.append(next_node)
+            for neighbour in next_node.adjacent:
+                if neighbour not in visited:
+                    visited.add(neighbour)
+                    queue.append(neighbour)
+        components.append(new_component)
+    print("\n  Components : {:8}".format(len(components)))
+    for (i, component) in enumerate(components):
+        print("    Component: {:8}".format(i))
+        edges = math.floor(sum(len(n.adjacent) for n in component) / 2)
+        density = edges / len(component) / (len(component) + 1)
+        print("    Nodes      : {:8}\n    Edges      : {:8}\n    Density    : {:8.2%}".format(len(component),
+                                                                                              edges,
+                                                                                              density))
+        print("    Expected maximal cliques in random graph = {}".format(
+            expected_maximal_cliques_in_random_graph(len(component), density)))
+
+
+def expected_maximal_cliques_in_random_graph(nodes: int, edge_probability: float) -> float:
+    last_term = nodes * math.pow(1 - edge_probability, nodes - 1)
+    expected_maximal = last_term
+    for d in range(1, nodes):
+        mult = (nodes - d) / (d + 1) * math.pow(edge_probability, d)
+        mult *= math.pow(1 - math.pow(edge_probability, d + 1), nodes - d - 1)
+        mult /= math.pow(1 - math.pow(edge_probability, d), nodes - d)
+        new_term = last_term * mult
+        expected_maximal += new_term
+        last_term = new_term
+    return expected_maximal
