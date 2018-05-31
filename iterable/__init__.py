d7ca14c3418ffca6e3ae4ce1635c5936bf904772
Niels-Christian F. Bagger <nc@fink.solutions>
2018-05-31 21:58:41 +0200

    Added function to get the minimum element of an iterable

--- iterable/__init__.py
+++ iterable/__init__.py
@@ -1,2 +1,13 @@
-def first(iterable):
-    return next(iter(iterable))
+def first(iterable, key=lambda x: True, default=None):
+    for element in iterable:
+        if key(element):
+            return element
+    return default
+
+
+def get_min_element(iterable, key=lambda x: x):
+    min_element = first(iterable)
+    for element in iterable:
+        if key(element) < key(min_element):
+            min_element = element
+    return min_element
