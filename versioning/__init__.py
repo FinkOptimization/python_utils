4b4440774a4874869e71da803a77a6a1b4e99937
Niels-Christian F. Bagger <nc@fink.solutions>
2018-06-08 14:36:19 +0200

    Added the new method to init

--- versioning/__init__.py
+++ versioning/__init__.py
@@ -1,3 +1,3 @@
 from .minimal_ext_cmd import minimal_ext_cmd
 from .pairing import n_pairing, reverse_n_pairing
-from .git_version import git_version, MAIN_BRANCHES
+from .git_version import MAIN_BRANCHES, git_version, update_git_version
